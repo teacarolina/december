aa95003bb3dffc31dfb6b32ba9178d1ffb4bea24
teacarolina <teacarolina@icloud.com>
2020-12-04 13:54:06 +0100

    new js files for cards

---
+++ cardsIndex.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+    var name = localStorage.getItem("nameValue")
+    var description = localStorage.getItem("descriptionValue")
+    var price = localStorage.getItem("priceValue")
+
+    //created a div that will be the container for the card 
+    var div = document.createElement("div")
+    div.setAttribute("class", "card")
+    //selected where the created div should be placed 
+    var section = document.querySelector(".landingpage-section")
+    section.appendChild(div)
+
+    //created h1 that stores the input from name field and stored h1 in div
+    var h1 = document.createElement("h1")
+    h1.innerHTML = name
+    div.appendChild(h1)
+
+     //created p that stores the input from the description field and stored p in div
+    var pDescription = document.createElement("p")
+    pDescription.innerHTML = description
+    div.appendChild(pDescription)
+
+     //created p that stores the input from the price field and stored p in div
+    var pPrice = document.createElement("p")
+    pPrice.setAttribute("class", "price")
+    pPrice.innerHTML = price
+    div.appendChild(pPrice)
+
+    //created p and button and stored p in div and button in p, added text to the button with innerHTML
+    var pButton = document.createElement("p")
+    var divButton = document.createElement("button")
+    divButton.innerHTML = "Add to cart"
+    div.appendChild(pButton)
+    pButton.appendChild(divButton)
+    })
