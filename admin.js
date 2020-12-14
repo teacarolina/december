fd019147f6b22d04ff1c6f129eb9cff431e62953
teacarolina <teacarolina@icloud.com>
2020-12-14 11:48:46 +0100

    small changes

--- admin.js
+++ admin.js
@@ -1,8 +1,10 @@
+//function to save admin input in local storage 
 function save(){
     //new data input from admin 
     var name = document.querySelector("#product--name").value;
     var description = document.querySelector("#product--description").value;
     var price = document.querySelector("#product--price").value;
+    //to give each card a specific id (random number 0-1)
     var id = Math.random();
 
     //storing an array in local storage
@@ -10,25 +12,26 @@ function save(){
         localStorage.setItem("data", "[]");
     }
 
-    //old data input pushed into array
+    //old data input pushed into array so no data is lost
     var old_data = JSON.parse(localStorage.getItem("data"));
     old_data.push({name, description, price, id});
 
-    //storing the array with the old data as well
+    //storing the array with the new and old data
     localStorage.setItem("data", JSON.stringify(old_data)); 
     
     formReset()
 }
 
-//to reset input fields after pressing button submit
+//function to reset input fields after pressing button submit
 function formReset() {
     document.querySelector(".input--form").reset();
 }
 
-//to remove object from localstorage
+//function to remove object from localstorage
 function removeElement(id) {  
         //get localstorage objects, filter with id and deleting specific id
         var dataCopy = JSON.parse(localStorage.getItem("data"))
+        //filter specific id and if its not this id we should keep information
         var newData = dataCopy.filter(item => item.id !== id)
         //storing again in local storage without deleted id 
         localStorage.setItem("data",JSON.stringify (newData)); 
@@ -36,9 +39,9 @@ function removeElement(id) {
         location.reload();
   }
 
-//to edit object in local storage
+//function to edit object in local storage
 function editElement(id) {
-    //prompt alert to add value to
+    //prompt alert to add new values 
     var newName = prompt("Please enter new name")
     var newDescription = prompt("Please enter new description")
     var newPrice = prompt("Please enter new price")
@@ -58,12 +61,13 @@ function editElement(id) {
     location.reload();
 }
 
+//function that creates the cards
 function view() { 
     //choosing where the input should appear
     var adminPage = document.querySelector(".admin-index")
     var homePage = document.querySelector(".landingpage-section")
-    //if we have an item in local storage named data and it´s not empty and a section called homePage it should
-    //be added to this section
+    //if we have an item in local storage named data and it´s not empty and a section called homePage/adminPage 
+    //exists it should be added to this section
     if(localStorage.getItem("data") != null && homePage && adminPage){
         var test = JSON.parse(localStorage.getItem("data")) 
         //collecting the values stored in objects in array 
@@ -72,7 +76,7 @@ function view() {
         homePage.innerHTML += `
         <section class="landingpage-section">
         <div class="card">  
-        <img class="bildtest" src="https://images.unsplash.com/photo-1588099768550-4014589e03e0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80">
+        <img class="bildtest" src="https://images.unsplash.com/photo-1521774971864-62e842046145?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80">
         <h1>${item.name}</h1>
         <p class="price">${item.price}</p>
         <p>${item.description}</p>
@@ -84,6 +88,7 @@ function view() {
     </section>
         `});   
     }
+    //if first statement is false this will run if its true
     else if(localStorage.getItem("data") != null && homePage){
         var test = JSON.parse(localStorage.getItem("data")) 
         //collecting the values stored in objects in array 
@@ -92,7 +97,7 @@ function view() {
         homePage.innerHTML += `
         <section class="landingpage-section">
         <div class="card">  
-        <img class="bildtest" src="https://images.unsplash.com/photo-1588099768550-4014589e03e0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=634&q=80">
+        <img class="bildtest" src="https://images.unsplash.com/photo-1521774971864-62e842046145?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80">
         <h1 class="product-title">${item.name}</h1>
         <p class="product-price">${item.price}</p>
         <p>${item.description}</p>
