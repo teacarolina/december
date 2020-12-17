5ec73c4cfa0db4f0c755614211fb2ea1da1d4c79
Richard <richard.santarinareyes@medieinstitutet.se>
2020-12-17 15:15:58 +0100

    just a little design

--- admin.js
+++ admin.js
@@ -1,7 +1,9 @@
 async function saveUrls() {
-  const response = await fetch("https://api.unsplash.com/photos/random/?client_id=hoi7Zos7S4lDzSm4l4MQ3P4apEmBUDWctKoocFTwnwY")
-  const dataUrls = await response.json()
-  var pictureUrls = dataUrls.urls.small
+  const response = await fetch(
+    "https://api.unsplash.com/photos/random/?client_id=hoi7Zos7S4lDzSm4l4MQ3P4apEmBUDWctKoocFTwnwY"
+  );
+  const dataUrls = await response.json();
+  var pictureUrls = dataUrls.urls.small;
   //console.log(picture)
   //storing an array in local storage
   if (localStorage.getItem("urls") == null) {
@@ -24,16 +26,16 @@ function save() {
   //to give each card a specific id (random number 0-1)
   var id = Math.random();
 
-    //console.log(id)
-  
+  //console.log(id)
+
   //storing an array in local storage
   if (localStorage.getItem("data") == null) {
     localStorage.setItem("data", "[]");
   }
   //old data input pushed into array so no data is lost
   var old_data = JSON.parse(localStorage.getItem("data"));
-  old_data.push({ name, description, price, id }); 
-  
+  old_data.push({ name, description, price, id });
+
   //storing the array with the new and old data
   localStorage.setItem("data", JSON.stringify(old_data));
 
@@ -61,66 +63,64 @@ function removeElement(id) {
 //fungerande error, fråga rakib om det här!
 //function to edit object in local storage
 function editElement(id) {
-    //prompt alert to add new values 
-    var newName = prompt("Please enter new name")
-    var newDescription = prompt("Please enter new description")
-    var newPrice = prompt("Please enter new price")
-    //get localstorage where previous input is saved
-    var editData = JSON.parse(localStorage.getItem("data"))
-    //for loop checking id for specific object, choosing that object and replacing
-    //with new input from prompt alert
+  //prompt alert to add new values
+  var newName = prompt("Please enter new name");
+  var newDescription = prompt("Please enter new description");
+  var newPrice = prompt("Please enter new price");
+  //get localstorage where previous input is saved
+  var editData = JSON.parse(localStorage.getItem("data"));
+  //for loop checking id for specific object, choosing that object and replacing
+  //with new input from prompt alert
 
-    for (var i in editData) {
-        if (editData[i].id === id) {
-           editData[i].name = newName 
-           editData[i].description = newDescription
-           editData[i].price = newPrice
-           //saving new data to localstorage
-           
-        }
+  for (var i in editData) {
+    if (editData[i].id === id) {
+      editData[i].name = newName;
+      editData[i].description = newDescription;
+      editData[i].price = newPrice;
+      //saving new data to localstorage
     }
-    //saving new data to localstorage, is outside the loop so we don't
-    //define what edit data is every time the loop runs
-    localStorage.setItem("data", JSON.stringify(editData))
+  }
+  //saving new data to localstorage, is outside the loop so we don't
+  //define what edit data is every time the loop runs
+  localStorage.setItem("data", JSON.stringify(editData));
   view();
 }
 //editData = localStorage.setItem("data", JSON.stringify(editData))
 
 //function that creates the cards
-function view() { 
-    //choosing where the input should appear
-    var adminPage = document.querySelector(".admin-index")
-    var homePage = document.querySelector(".landingpage-section")
-    //Kollar om vi misslyckats att hämta homePage, finns ej slutar vi
-    if(homePage === null){
-        console.log('hittar ej homepage')
-        return;
-    }
-    //tar bort tidigare produkter så de inte läggs in om och om igen
-    homePage.innerHTML = '';
-    var dataProductInfo = localStorage.getItem("data");
+function view() {
+  //choosing where the input should appear
+  var adminPage = document.querySelector(".admin-index");
+  var homePage = document.querySelector(".landingpage-section");
+  //Kollar om vi misslyckats att hämta homePage, finns ej slutar vi
+  if (homePage === null) {
+    console.log("hittar ej homepage");
+    return;
+  }
+  //tar bort tidigare produkter så de inte läggs in om och om igen
+  homePage.innerHTML = "";
+  var dataProductInfo = localStorage.getItem("data");
 
-    //Kolla om dataproduct är null, då hoppar vi ur funktionen eftersom något är fel
-    if( dataProductInfo == null) {
-        console.log('hello');
-        return;
-    }
-    var test = JSON.parse(localStorage.getItem("data")) 
-    var testUrl = JSON.parse(localStorage.getItem("urls"))
-    //if we have an item in local storage named data and it´s not empty and a section called homePage/adminPage 
-    //exists it should be added to this section
-    if(adminPage){
-       
-        //item.pictureUrls[index] om vi map (item, index) måste vara en array pictureUrls 
-        //collecting the values stored in objects in array 
-        //old image url:
-        //https://images.unsplash.com/photo-1521774971864-62e842046145?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
-        Object.values(test, testUrl).map(item => { 
-        //choosing how they should appear on page
-        homePage.innerHTML += `
+  //Kolla om dataproduct är null, då hoppar vi ur funktionen eftersom något är fel
+  if (dataProductInfo == null) {
+    console.log("hello");
+    return;
+  }
+  var test = JSON.parse(localStorage.getItem("data"));
+  var testUrl = JSON.parse(localStorage.getItem("urls"));
+  //if we have an item in local storage named data and it´s not empty and a section called homePage/adminPage
+  //exists it should be added to this section
+  if (adminPage) {
+    //item.pictureUrls[index] om vi map (item, index) måste vara en array pictureUrls
+    //collecting the values stored in objects in array
+    //old image url:
+    //https://images.unsplash.com/photo-1521774971864-62e842046145?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80"
+    Object.values(test, testUrl).map((item) => {
+      //choosing how they should appear on page
+      homePage.innerHTML += `
         <section class="landingpage-section">
         <div class="card">  
-        <img class="product-image" src="${item.pictureUrls}">
+        <img class="product-image" src="">
         <h1>${item.name}</h1>
         <p class="price">${item.price}</p>
         <p>${item.description}</p>
@@ -128,19 +128,20 @@ function view() {
         <button onclick="removeElement(${item.id})" class="admin-remove" type="button">REMOVE</button>
         <button onclick="editElement(${item.id})" class="admin-edit" type="button">EDIT</button>
       </div>
-     
+    
     </section>
-        `});   
-    }
-    //if first statement is false this will run if its true
-    else { 
-        //collecting the values stored in objects in array 
-        Object.values(test).map(item => { 
-        //choosing how they should appear on page
-        homePage.innerHTML += `
+        `;
+    });
+  }
+  //if first statement is false this will run if its true
+  else {
+    //collecting the values stored in objects in array
+    Object.values(test).map((item) => {
+      //choosing how they should appear on page
+      homePage.innerHTML += `
         <section class="landingpage-section">
         <div class="card">  
-        <img class="product-image" src="${item.pictureUrls}">
+        <img class="product-image" src="">
         <h1 class="product-title">${item.name}</h1>
         <p class="product-price">${item.price}</p>
         <p>${item.description}</p>
